11e6294c6dcd3d8596217693708e9abd4d438dc0
flywithyou <1254166265@qq.com>
2019-10-04 16:59:43 +0800

    local update 05

--- src/main/java/com/cy/dao/MytestClass.java
+++ src/main/java/com/cy/dao/MytestClass.java
@@ -6,5 +6,6 @@ public class MytestClass {
 		System.out.println("hello ,github server002!");
 		System.out.println("github server update003");
 		System.out.println("github 服务器 update004");
+		System.out.println("hello ,github server005!");
 	}
 }
